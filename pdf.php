7c1b64204d352080fa821b6c73dc2c0ba08fd8cc
Bruno Roberto Gomes <bruno@brgomes.com>
2021-02-25 11:40:34 -0300

    Update documentation

--- pdf.php
+++ pdf.php
@@ -1,24 +1,11 @@
 <?php
 
 /**
-Para converter o certificado .pfx em arquivos .pem público e privado
+Para converter o certificado .pfx em arquivo .crt
 
-CERTIFICADO .pfx PARA ARQUIVO .pem PRIVADO:
-openssl pkcs12 -in filename.pfx -nocerts -out PRIVATE_KEY.pem
+openssl pkcs12 -in PFX_FILE.pfx -out CERTIFICATE.crt -nodes
 
-CERTIFICADO .pfx PARA ARQUIVO .pem PÚBLICO:
-openssl pkcs12 -in filename.pfx -clcerts -nokeys -out PUBLIC_CERT.pem
-
-Ambos os comandos acima precisam da senha do certificado.
-
-Ao converter para arquivo .pem PRIVADO, é necessário informar uma senha,
-que é usada no código abaixo através da constante CERT_PASSWORD.
-
-Para usar o código abaixo em produção basta o NTI gerar os arquivos
-.pem público e privado do certificado e colocar no servidor. O usuário
-que tiver permissão para gerar documentos assinados poderá fazê-lo
-independente de ter chave ou não, pois o certificado já estará
-guardado no servidor.
+O comando acima precisa da senha do certificado.
 **/
 
 ini_set('display_errors', 1);
